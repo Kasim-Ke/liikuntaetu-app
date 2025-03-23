3bd2b72023b18209ad986eb23efbcddf93557c87
Kasim Ke <shkpostisi@esimerkki.com>
2025-03-23 19:13:15 +0200

    Lisää client-kansion puuttuvat tiedostot

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import Kilometer from "./models/kilometers.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
